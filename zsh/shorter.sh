9e92f3db94a72bf326ab24874659430eee6e4003
ohsirawat <ohsirawat@gmail.com>
2021-07-29 17:01:08 +0700

    add short source zshrc

--- zsh/shorter.sh
+++ zsh/shorter.sh
@@ -2,3 +2,4 @@
 alias Mrosdep="rosdep install --from-path src --ignore-src -y -r"
 alias Mcolcon="colcon build --packages-select $1"
 alias Mws="source install/setup.zsh"
+alias zshrc="source ~/.zshrc"
