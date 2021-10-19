ada866fd49beb3225f4645121dff02cb0e40558d
SirawatS <ohsirawat@gmail.com>
2021-10-19 12:59:49 +0700

    add macro

--- zsh/shorter.sh
+++ zsh/shorter.sh
@@ -2,4 +2,11 @@
 alias Mrosdep="rosdep install --from-path src --ignore-src -y -r"
 alias Mcolcon="colcon build --packages-select $1"
 alias Mws="source install/setup.zsh"
-alias zshrc="source ~/.zshrc"
+alias Mzshrc="source ~/.zshrc"
+alias Mcolcon="colcon build --packages-select $1"
+
+## Docker
+alias Mdocker_rmi_none="docker rmi $(docker images -a --filter "dangling=true" -q --no-trunc) -f"
+
+## Python
+alias Mpyclean="find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete"
