6f6d20c0545c97c582830b8aad3713ecad66e57e
SirawatS <ohsirawat@gmail.com>
2021-11-09 18:35:02 +0700

    fix bug alias

--- zsh/shorter.sh
+++ zsh/shorter.sh
@@ -6,7 +6,12 @@ alias Mzshrc="source ~/.zshrc"
 alias Mcolcon="colcon build --packages-select $1"
 
 ## Docker
-alias Mdocker_rmi_none="docker rmi $(docker images -a --filter "dangling=true" -q --no-trunc) -f"
+
+Mdocker_rmi_none () {
+  local list=$(docker images -a --filter "dangling=true" -q --no-trunc)
+  docker rmi $list -f
+}
+
 
 ## Python
 alias Mpyclean="find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete"
