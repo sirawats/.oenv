9f61de40ac97a8637d5265f50f2894c3fbdc5225
SirawatS <ohsirawat@gmail.com>
2022-01-12 23:18:44 +0700

    update

--- utils/mp4togif.sh
+++ utils/mp4togif.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-ffmpeg \
-  -i opengl-rotating-triangle.mp4 \ # input video
-  -r 15 \ # sampling FPS
-  -vf scale=512:-1 \ # resolution scaling
-  -ss 00:00:03 -to 00:00:06 \ # start time to end time
-  opengl-rotating-triangle.gif # output 
+# ffmpeg \
+#   -i opengl-rotating-triangle.mp4 \ # input video
+#   -r 15 \ # sampling FPS
+#   -vf scale=512:-1 \ # resolution scaling
+#   -ss 00:00:03 -to 00:00:06 \ # start time to end time
+#   opengl-rotating-triangle.gif # output 
+
+ffmpeg -i $1 -r 15 -vf scale=512:-1 $2 
