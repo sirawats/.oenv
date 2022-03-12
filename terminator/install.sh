bdaaf373a215e202b7762ce676eb7611b201e511
SirawatS <ohsirawat@gmail.com>
2022-03-12 20:34:23 +0700

    update font

--- terminator/install.sh
+++ terminator/install.sh
@@ -3,3 +3,6 @@ echo "Run 'sudo apt install terminator' ..."
 sudo apt install terminator
 echo "Copy terminator to ~/.config "
 cp -r terminator ~/.config/.
+echo "Install JetBrainsMono NF fonts"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -p /tmp
+unzip /tmp/JetBrainsMono -d ~/.local/share/fonts
