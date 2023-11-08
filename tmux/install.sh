b8388daf49b605ba47c6abecb7b126c2444eacdb
o-sirawat <ohsirawat@gmail.com>
2023-11-08 10:15:09 +0700

    update

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
-RED="\e[1;31m"
-BLUE="\e[1;34m"
-GREEN="\e[1;32m"
-END="\e[0m"
+RED=$'\e[1;31m'
+BLUE=$'\e[1;34m'
+GREEN=$'\e[1;32m'
+END=$'\e[0m'
 
-echo -e "${BLUE}Install tmux${END}"
-sudo apt install -y tmux
-echo -e "${BLUE}Install .tmux${END}"
-cd
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-curl -o ~/.tmux.conf.local -L https://raw.githubusercontent.com/leossok/.leorc/master/tmux/.tmux.conf.local
+
+read -r -p "${BLUE}Install tmux [y/N]${END}" response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+  sudo apt update
+  sudo apt install -y tmux
+fi
+
+read -r -p "${BLUE}Install o-sirawat/.tmux [y/N]${END}" response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+  cd
+  git clone https://github.com/gpakosz/.tmux.git
+  ln -s -f .tmux/.tmux.conf
+  curl -o ~/.tmux.conf.local -L https://raw.githubusercontent.com/o-sirawat/.oenv/master/tmux/.tmux.conf.local
+fi
+
 echo -e "${BLUE}Finish!${END}"
-
-
