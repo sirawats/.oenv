7a66d2d4c6686c83b3e067af5b0a6a8bfcc2ebd1
SirawatS <ohsirawat@gmail.com>
2024-02-03 16:55:04 +0700

    fix error every excute

--- utils/src/zshrc.sh
+++ utils/src/zshrc.sh
@@ -2,10 +2,3 @@
 
 # Source the ~/.zshrc file
 source ~/.zshrc
-
-# Check if sourcing was successful
-if [ $? -eq 0 ]; then
-    echo "Sourced ~/.zshrc successfully."
-else
-    echo "Error: Failed to source ~/.zshrc"
-fi
